bf8b798f2bbff894c1f298f9f1bc7f6a1019d0bb
ogeret <maxandre.ogeret@ut.ee>
2021-04-13 00:05:15 +0300

    autoware master, checked out op packages

--- lidar_kf_contour_track/include/PolygonGenerator.h
+++ lidar_kf_contour_track/include/PolygonGenerator.h
@@ -14,6 +14,12 @@
  * limitations under the License.
  */
 
+
+/// \file  PolygonGenerator.h
+/// \brief Generate convex hull from point cloud cluster of detected object
+/// \author Hatem Darweesh
+/// \date Nov 2, 2016
+
 #ifndef OP_POLYGONGENERATOR_H_
 #define OP_POLYGONGENERATOR_H_
 
@@ -31,58 +37,58 @@ namespace ContourTrackerNS
 class QuarterView
 {
 public:
-  int id;
-  int min_ang;
-  int max_ang;
-  PlannerHNS::WayPoint max_from_center;
-  bool bFirst;
+	int id;
+	int min_ang;
+	int max_ang;
+	PlannerHNS::WayPoint max_from_center;
+	bool bFirst;
 
-  QuarterView(const int& min_a, const int& max_a, const int& index)
-  {
-    min_ang = min_a;
-    max_ang = max_a;
-    id = index;
-    bFirst = true;
-  }
+	QuarterView(const int& min_a, const int& max_a, const int& index)
+	{
+		min_ang = min_a;
+		max_ang = max_a;
+		id = index;
+		bFirst = true;
+	}
 
-  void InitQuarterView(const int& min_a, const int& max_a, const int& index)
-  {
-    min_ang = min_a;
-    max_ang = max_a;
-    id = index;
-    bFirst = true;
-  }
+	void InitQuarterView(const int& min_a, const int& max_a, const int& index)
+	{
+		min_ang = min_a;
+		max_ang = max_a;
+		id = index;
+		bFirst = true;
+	}
 
-  void ResetQuarterView()
-  {
-    bFirst = true;
-  }
+	void ResetQuarterView()
+	{
+		bFirst = true;
+	}
 
-  bool UpdateQuarterView(const PlannerHNS::WayPoint& v)
-  {
-    if(v.pos.a <= min_ang || v.pos.a > max_ang)
-      return false;
+	bool UpdateQuarterView(const PlannerHNS::WayPoint& v)
+	{
+		if(v.pos.a <= min_ang || v.pos.a > max_ang)
+			return false;
 
-    if(bFirst)
-    {
-      max_from_center = v;
-      bFirst = false;
-    }
-    else if(v.cost > max_from_center.cost)
-      max_from_center = v;
+		if(bFirst)
+		{
+			max_from_center = v;
+			bFirst = false;
+		}
+		else if(v.distanceCost > max_from_center.distanceCost)
+			max_from_center = v;
 
-    return true;
-  }
+		return true;
+	}
 
-  bool GetMaxPoint(PlannerHNS::WayPoint& maxPoint)
-  {
-    if(bFirst)
-      return false;
-    else
-      maxPoint = max_from_center;
+	bool GetMaxPoint(PlannerHNS::WayPoint& maxPoint)
+	{
+		if(bFirst)
+			return false;
+		else
+			maxPoint = max_from_center;
 
-    return true;
-  }
+		return true;
+	}
 };
 
 class PolygonGenerator
@@ -90,14 +96,15 @@ class PolygonGenerator
 
 public:
 
-  PlannerHNS::GPSPoint m_Centroid;
-  std::vector<QuarterView> m_Quarters;
-  std::vector<PlannerHNS::GPSPoint> m_Polygon;
+	PlannerHNS::GPSPoint m_Centroid;
+	std::vector<QuarterView> m_Quarters;
+	std::vector<PlannerHNS::GPSPoint> m_Polygon;
+	std::vector<PlannerHNS::GPSPoint> m_PolygonRes;
 
-  PolygonGenerator(int nQuarters);
-  virtual ~PolygonGenerator();
-  std::vector<QuarterView> CreateQuarterViews(const int& nResolution);
-  std::vector<PlannerHNS::GPSPoint> EstimateClusterPolygon(const pcl::PointCloud<pcl::PointXYZ>& cluster, const PlannerHNS::GPSPoint& original_centroid, PlannerHNS::GPSPoint& new_centroid, const double& polygon_resolution = 1.0);
+	PolygonGenerator(int nQuarters);
+	virtual ~PolygonGenerator();
+	std::vector<QuarterView> CreateQuarterViews(const int& nResolution);
+	std::vector<PlannerHNS::GPSPoint> EstimateClusterPolygon(const pcl::PointCloud<pcl::PointXYZ>& cluster, const PlannerHNS::GPSPoint& original_centroid, PlannerHNS::GPSPoint& new_centroid, const double& polygon_resolution = 1.0);
 };
 
 } /* namespace PlannerXNS */
--- lidar_kf_contour_track/include/lidar_kf_contour_track_core.h
+++ lidar_kf_contour_track/include/lidar_kf_contour_track_core.h
@@ -37,163 +37,218 @@
 #include "SimpleTracker.h"
 #include "PolygonGenerator.h"
 
+#include <std_msgs/String.h>
+#include <tf/transform_listener.h>
+#include <autoware_can_msgs/CANInfo.h>
+#include <nav_msgs/Odometry.h>
+#include <geometry_msgs/TwistStamped.h>
+#include <jsk_recognition_msgs/BoundingBoxArray.h>
 #include <autoware_msgs/CloudClusterArray.h>
 #include <autoware_msgs/DetectedObjectArray.h>
+#include <autoware_msgs/VehicleStatus.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <visualization_msgs/MarkerArray.h>
+#include <autoware_lanelet2_msgs/MapBin.h>
 
 
 
 namespace ContourTrackerNS
 {
 
+enum MAP_FILTER_TYPE
+{
+	FILTER_DISABLE,
+	FILTER_BOUNDARY,
+	FILTER_CENTERLINES
+	};
+
 class PerceptionParams
 {
 public:
 
-  double   VehicleWidth;
-  double   VehicleLength;
-  double   DetectionRadius;
-  double   MinObjSize;
-  double   MaxObjSize;
-  double  nQuarters;
-  double   PolygonRes;
-  TRACKING_TYPE  trackingType; // 0 association only , 1 simple tracking, 2 contour based tracking
-  bool    bEnableSimulation;
-  bool   bEnableStepByStep;
-  bool   bEnableLogging;
-  bool bEnableTTC;
-  bool bEnableLaneChange;
+	double 	VehicleWidth;
+	double 	VehicleLength;
+	double 	DetectionRadius;
+	double 	MinObjSize;
+	double 	MaxObjSize;
+	double  nQuarters;
+	double 	PolygonRes;
+	TRACKING_TYPE	trackingType; // 0 association only , 1 simple tracking, 2 contour based tracking
+	bool bEnableSimulation;
+	bool bEnableStepByStep;
+	bool bEnableLogging;
+	bool bEnableTTC;
+	bool bEnableLaneChange;
+	bool bEnableBenchmark;
+	bool bEnableInternalVisualization;
+	bool bUseDetectionHulls;
+	MAP_FILTER_TYPE filterType;
+	double centerlineFilterDistance;
 
-  PerceptionParams()
-  {
-    VehicleWidth =0;
-    VehicleLength =0;
-    DetectionRadius =0;
-    MinObjSize =0;
-    MaxObjSize =0;
-    nQuarters = 0;
-    PolygonRes = 0;
-    trackingType = SIMPLE_TRACKER;
-    bEnableStepByStep = false;
-    bEnableSimulation = false;
-    bEnableLogging = false;
-    bEnableTTC = false;
-    bEnableLaneChange = false;
-  }
+	PerceptionParams()
+	{
+		VehicleWidth =0;
+		VehicleLength =0;
+		DetectionRadius =0;
+		MinObjSize =0;
+		MaxObjSize =0;
+		nQuarters = 0;
+		PolygonRes = 0;
+		trackingType = SIMPLE_TRACKER;
+		bEnableStepByStep = false;
+		bEnableSimulation = false;
+		bEnableLogging = false;
+		bEnableTTC = false;
+		bEnableLaneChange = false;
+		bEnableBenchmark = false;
+		bEnableInternalVisualization = false;
+		bUseDetectionHulls = false;
+		filterType = FILTER_DISABLE;
+		centerlineFilterDistance = 1.5;
+	}
 };
 
 class ContourTracker
 {
 protected:
-  std::vector<PlannerHNS::DetectedObject> m_OriginalClusters;
-  autoware_msgs::DetectedObjectArray m_OutPutResults;
-  bool bNewClusters;
-  PlannerHNS::WayPoint m_CurrentPos;
-  bool bNewCurrentPos;
-  PerceptionParams m_Params;
-  SimpleTracker m_ObstacleTracking;
+	std::vector<PlannerHNS::DetectedObject> m_OriginalClusters;
+	autoware_msgs::DetectedObjectArray m_OutPutResults;
+	bool bNewClusters;
+	PlannerHNS::WayPoint m_CurrentPos;
+	PlannerHNS::VehicleState m_VehicleStatus;
+	int m_VelocitySource;
+	bool bNewCurrentPos;
+	PerceptionParams m_Params;
+	SimpleTracker m_ObstacleTracking;
 
-  //Visualization Section
-  int m_nDummyObjPerRep;
-  int m_nDetectedObjRepresentations;
-  std::vector<visualization_msgs::MarkerArray> m_DetectedPolygonsDummy;
-  std::vector<visualization_msgs::MarkerArray> m_DetectedPolygonsActual;
-  visualization_msgs::MarkerArray m_DetectedPolygonsAllMarkers;
-  visualization_msgs::MarkerArray m_DetectionCircles;
+	//Visualization Section
+	int m_nDummyObjPerRep;
+	int m_nDetectedObjRepresentations;
+	std::vector<visualization_msgs::MarkerArray> m_DetectedPolygonsDummy;
+	std::vector<visualization_msgs::MarkerArray> m_DetectedPolygonsActual;
+	visualization_msgs::MarkerArray m_DetectedPolygonsAllMarkers;
+	visualization_msgs::MarkerArray m_DetectionCircles;
 
-  std::vector<visualization_msgs::MarkerArray> m_MatchingInfoDummy;
-  std::vector<visualization_msgs::MarkerArray> m_MatchingInfoActual;
-
-
-  visualization_msgs::MarkerArray m_TTC_Path;
-  visualization_msgs::Marker m_TTC_Info;
-
-  std::vector<std::string>    m_LogData;
-  PlannerHNS::MAP_SOURCE_TYPE m_MapType;
-  std::string m_MapPath;
-  PlannerHNS::RoadNetwork m_Map;
-  bool bMap;
-  double m_MapFilterDistance;
-
-  std::vector<PlannerHNS::Lane*> m_ClosestLanesList;
-
-  int m_nOriginalPoints;
-  int m_nContourPoints;
-  double m_FilteringTime;
-  double m_PolyEstimationTime;
-  double m_tracking_time;
-  double m_dt;
-  struct timespec  m_loop_timer;
-
-  //ROS subscribers
-  ros::NodeHandle nh;
-
-  //define publishers
-  ros::Publisher pub_AllTrackedObjects;
-
-  ros::Publisher pub_DetectedPolygonsRviz;
-  ros::Publisher pub_TrackedObstaclesRviz;
-  ros::Publisher pub_TTC_PathRviz;
-
-  // define subscribers.
-  ros::Subscriber sub_cloud_clusters;
-  ros::Subscriber sub_current_pose ;
+	std::vector<visualization_msgs::MarkerArray> m_MatchingInfoDummy;
+	std::vector<visualization_msgs::MarkerArray> m_MatchingInfoActual;
 
 
-  // Callback function for subscriber.
-  void callbackGetCloudClusters(const autoware_msgs::CloudClusterArrayConstPtr &msg);
-  void callbackGetCurrentPose(const geometry_msgs::PoseStampedConstPtr& msg);
+	visualization_msgs::MarkerArray m_TTC_Path;
+	visualization_msgs::Marker m_TTC_Info;
 
-  //Helper Functions
-  void VisualizeLocalTracking();
-  void ImportCloudClusters(const autoware_msgs::CloudClusterArrayConstPtr& msg, std::vector<PlannerHNS::DetectedObject>& originalClusters);
-  bool IsCar(const PlannerHNS::DetectedObject& obj, const PlannerHNS::WayPoint& currState, PlannerHNS::RoadNetwork& map);
-  void CalculateTTC(const std::vector<PlannerHNS::DetectedObject>& objs, const PlannerHNS::WayPoint& currState, PlannerHNS::RoadNetwork& map);
-  void GetFrontTrajectories(std::vector<PlannerHNS::Lane*>& lanes, const PlannerHNS::WayPoint& currState, const double& max_distance, std::vector<std::vector<PlannerHNS::WayPoint> >& trajectories);
-  void ReadNodeParams();
-  void ReadCommonParams();
-  void LogAndSend();
+	std::vector<std::string>    m_LogData;
+	PlannerHNS::MAP_SOURCE_TYPE m_MapType;
+	std::string m_MapPath;
+	PlannerHNS::RoadNetwork m_Map;
+	bool bMap;
+	bool bCommonParams;
+	std::string m_ExperimentFolderName;
+
+	std::vector<PlannerHNS::Lane*> m_ClosestLanesList;
+
+	int m_nOriginalPoints;
+	int m_nContourPoints;
+	double m_FilteringTime;
+	double m_PolyEstimationTime;
+	double m_tracking_time;
+	double m_dt;
+	struct timespec  m_loop_timer;
+	int frame_count_;
+	std::string kitti_data_dir_;
+	std::string result_file_path_;
+	//std::string pointcloud_frame ;
+	std::string target_tracking_frame;
+	std::string source_data_frame;
+	tf::TransformListener tf_listener;
+	tf::StampedTransform m_local2global;
+	std_msgs::Header m_InputHeader;
+
+	//ROS subscribers
+	ros::NodeHandle nh;
+
+	//define publishers
+	ros::Publisher pub_AllTrackedObjects;
+
+	ros::Publisher pub_DetectedPolygonsRviz;
+	//ros::Publisher pub_TrackedObstaclesRviz;
+	ros::Publisher pub_TTC_PathRviz;
+
+	// define subscribers.
+	ros::Subscriber sub_cloud_clusters;
+	ros::Subscriber sub_current_pose ;
+	ros::Subscriber sub_detected_objects;
+	ros::Subscriber sub_current_velocity;
+	ros::Subscriber sub_robot_odom;
+	ros::Subscriber sub_can_info;
+	ros::Subscriber sub_vehicle_status;
+	ros::Subscriber sub_map_file_name;
+
+
+	// Callback function for subscriber.
+	void callbackGetCloudClusters(const autoware_msgs::CloudClusterArrayConstPtr &msg);
+	void callbackGetCurrentPose(const geometry_msgs::PoseStampedConstPtr& msg);
+	void callbackGetDetectedObjects(const autoware_msgs::DetectedObjectArrayConstPtr& msg);
+	void callbackGetAutowareStatus(const geometry_msgs::TwistStampedConstPtr& msg);
+	void callbackGetCanInfo(const autoware_can_msgs::CANInfoConstPtr & msg);
+	void callbackGetRobotOdom(const nav_msgs::OdometryConstPtr& msg);
+	void callbackGetVehicleStatus(const autoware_msgs::VehicleStatusConstPtr & msg);
+
+	//Helper Functions
+	void VisualizeLocalTracking();
+	void ImportCloudClusters(const autoware_msgs::CloudClusterArray& msg, std::vector<PlannerHNS::DetectedObject>& originalClusters);
+	void ImportDetectedObjects(const autoware_msgs::DetectedObjectArray& msg, std::vector<PlannerHNS::DetectedObject>& originalClusters);
+	bool FilterByMap(const PlannerHNS::DetectedObject& obj, const PlannerHNS::WayPoint& currState, PlannerHNS::RoadNetwork& map);
+	bool FilterBySize(const PlannerHNS::DetectedObject& obj, const PlannerHNS::WayPoint& currState);
+	void CalculateTTC(const std::vector<PlannerHNS::DetectedObject>& objs, const PlannerHNS::WayPoint& currState, PlannerHNS::RoadNetwork& map);
+	void GetFrontTrajectories(std::vector<PlannerHNS::Lane*>& lanes, const PlannerHNS::WayPoint& currState, const double& max_distance, std::vector<std::vector<PlannerHNS::WayPoint> >& trajectories);
+	void transformPoseToGlobal(const std::string& src_frame, const std::string& dst_frame, const tf::StampedTransform& local2global, const autoware_msgs::CloudClusterArray& input, autoware_msgs::CloudClusterArray& transformed_input);
+	void ReadNodeParams();
+	void ReadCommonParams();
+	void Log();
+	void PostProcess();
+	void dumpResultText(autoware_msgs::DetectedObjectArray& detected_objects);
 
 public:
   ContourTracker();
   ~ContourTracker();
   void MainLoop();
 
-  //Mapping Section
+	//Mapping Section
+	UtilityHNS::MapRaw m_MapRaw;
+  	ros::Subscriber sub_bin_map;
+	ros::Subscriber sub_lanes;
+	ros::Subscriber sub_points;
+	ros::Subscriber sub_dt_lanes;
+	ros::Subscriber sub_intersect;
+	ros::Subscriber sup_area;
+	ros::Subscriber sub_lines;
+	ros::Subscriber sub_stop_line;
+	ros::Subscriber sub_signals;
+	ros::Subscriber sub_vectors;
+	ros::Subscriber sub_curbs;
+	ros::Subscriber sub_edges;
+	ros::Subscriber sub_way_areas;
+	ros::Subscriber sub_cross_walk;
+	ros::Subscriber sub_nodes;
 
-  UtilityHNS::MapRaw m_MapRaw;
-
-  ros::Subscriber sub_lanes;
-  ros::Subscriber sub_points;
-  ros::Subscriber sub_dt_lanes;
-  ros::Subscriber sub_intersect;
-  ros::Subscriber sup_area;
-  ros::Subscriber sub_lines;
-  ros::Subscriber sub_stop_line;
-  ros::Subscriber sub_signals;
-  ros::Subscriber sub_vectors;
-  ros::Subscriber sub_curbs;
-  ros::Subscriber sub_edges;
-  ros::Subscriber sub_way_areas;
-  ros::Subscriber sub_cross_walk;
-  ros::Subscriber sub_nodes;
-
-
-  void callbackGetVMLanes(const vector_map_msgs::LaneArray& msg);
-  void callbackGetVMPoints(const vector_map_msgs::PointArray& msg);
-  void callbackGetVMdtLanes(const vector_map_msgs::DTLaneArray& msg);
-  void callbackGetVMIntersections(const vector_map_msgs::CrossRoadArray& msg);
-  void callbackGetVMAreas(const vector_map_msgs::AreaArray& msg);
-  void callbackGetVMLines(const vector_map_msgs::LineArray& msg);
-  void callbackGetVMStopLines(const vector_map_msgs::StopLineArray& msg);
-  void callbackGetVMSignal(const vector_map_msgs::SignalArray& msg);
-  void callbackGetVMVectors(const vector_map_msgs::VectorArray& msg);
-  void callbackGetVMCurbs(const vector_map_msgs::CurbArray& msg);
-  void callbackGetVMRoadEdges(const vector_map_msgs::RoadEdgeArray& msg);
-  void callbackGetVMWayAreas(const vector_map_msgs::WayAreaArray& msg);
-  void callbackGetVMCrossWalks(const vector_map_msgs::CrossWalkArray& msg);
-  void callbackGetVMNodes(const vector_map_msgs::NodeArray& msg);
+	void callbackGetLanelet2(const autoware_lanelet2_msgs::MapBin& msg);
+	void callbackGetVMLanes(const vector_map_msgs::LaneArray& msg);
+	void callbackGetVMPoints(const vector_map_msgs::PointArray& msg);
+	void callbackGetVMdtLanes(const vector_map_msgs::DTLaneArray& msg);
+	void callbackGetVMIntersections(const vector_map_msgs::CrossRoadArray& msg);
+	void callbackGetVMAreas(const vector_map_msgs::AreaArray& msg);
+	void callbackGetVMLines(const vector_map_msgs::LineArray& msg);
+	void callbackGetVMStopLines(const vector_map_msgs::StopLineArray& msg);
+	void callbackGetVMSignal(const vector_map_msgs::SignalArray& msg);
+	void callbackGetVMVectors(const vector_map_msgs::VectorArray& msg);
+	void callbackGetVMCurbs(const vector_map_msgs::CurbArray& msg);
+	void callbackGetVMRoadEdges(const vector_map_msgs::RoadEdgeArray& msg);
+	void callbackGetVMWayAreas(const vector_map_msgs::WayAreaArray& msg);
+	void callbackGetVMCrossWalks(const vector_map_msgs::CrossWalkArray& msg);
+	void callbackGetVMNodes(const vector_map_msgs::NodeArray& msg);
+	void kmlMapFileNameCallback(const std_msgs::String& file_name);
+	void LoadKmlMap(const std::string& file_name);
+	void LoadMap();
 };
 
 }
