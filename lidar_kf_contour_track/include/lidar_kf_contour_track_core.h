867db46532c54ae2656fe271255f2a4494c77aaf
Hatem Darweesh <hatem.darweesh@gmail.com>
2021-10-05 15:26:24 +0900

    Aggregate ROS related redundant code for Map loading and Velocity topics in op_ros_helpers Add OpenPlanner parameter control basic class and first two paramers (rolloutsnumbers and rolloutsdensity)

--- lidar_kf_contour_track/include/lidar_kf_contour_track_core.h
+++ lidar_kf_contour_track/include/lidar_kf_contour_track_core.h
@@ -20,37 +20,20 @@
 // ROS includes
 #include <ros/ros.h>
 
-#include "vector_map_msgs/PointArray.h"
-#include "vector_map_msgs/LaneArray.h"
-#include "vector_map_msgs/NodeArray.h"
-#include "vector_map_msgs/StopLineArray.h"
-#include "vector_map_msgs/DTLaneArray.h"
-#include "vector_map_msgs/LineArray.h"
-#include "vector_map_msgs/AreaArray.h"
-#include "vector_map_msgs/SignalArray.h"
-#include "vector_map_msgs/StopLine.h"
-#include "vector_map_msgs/VectorArray.h"
-
 #include "op_planner/RoadNetwork.h"
 #include "op_planner/PlannerCommonDef.h"
 #include "op_planner/MatrixOperations.h"
 #include "SimpleTracker.h"
 #include "PolygonGenerator.h"
+#include "op_ros_helpers/ROSMapHandler.h"
+#include "op_ros_helpers/ROSVelocityHandler.h"
 
-#include <std_msgs/String.h>
 #include <tf/transform_listener.h>
-#include <autoware_can_msgs/CANInfo.h>
-#include <nav_msgs/Odometry.h>
-#include <geometry_msgs/TwistStamped.h>
 #include <jsk_recognition_msgs/BoundingBoxArray.h>
 #include <autoware_msgs/CloudClusterArray.h>
 #include <autoware_msgs/DetectedObjectArray.h>
-#include <autoware_msgs/VehicleStatus.h>
 #include <geometry_msgs/PoseStamped.h>
 #include <visualization_msgs/MarkerArray.h>
-#include <autoware_lanelet2_msgs/MapBin.h>
-
-
 
 namespace ContourTrackerNS
 {
@@ -116,7 +99,6 @@ protected:
 	bool bNewClusters;
 	PlannerHNS::WayPoint m_CurrentPos;
 	PlannerHNS::VehicleState m_VehicleStatus;
-	int m_VelocitySource;
 	bool bNewCurrentPos;
 	PerceptionParams m_Params;
 	SimpleTracker m_ObstacleTracking;
@@ -137,10 +119,9 @@ protected:
 	visualization_msgs::Marker m_TTC_Info;
 
 	std::vector<std::string>    m_LogData;
-	PlannerHNS::MAP_SOURCE_TYPE m_MapType;
-	std::string m_MapPath;
+	PlannerHNS::MapHandler m_MapHandler;
+	PlannerHNS::VelocityHandler m_VelHandler;
 	PlannerHNS::RoadNetwork m_Map;
-	bool bMap;
 	bool bCommonParams;
 	std::string m_ExperimentFolderName;
 
@@ -177,21 +158,11 @@ protected:
 	ros::Subscriber sub_cloud_clusters;
 	ros::Subscriber sub_current_pose ;
 	ros::Subscriber sub_detected_objects;
-	ros::Subscriber sub_current_velocity;
-	ros::Subscriber sub_robot_odom;
-	ros::Subscriber sub_can_info;
-	ros::Subscriber sub_vehicle_status;
-	ros::Subscriber sub_map_file_name;
-
 
 	// Callback function for subscriber.
 	void callbackGetCloudClusters(const autoware_msgs::CloudClusterArrayConstPtr &msg);
 	void callbackGetCurrentPose(const geometry_msgs::PoseStampedConstPtr& msg);
 	void callbackGetDetectedObjects(const autoware_msgs::DetectedObjectArrayConstPtr& msg);
-	void callbackGetAutowareStatus(const geometry_msgs::TwistStampedConstPtr& msg);
-	void callbackGetCanInfo(const autoware_can_msgs::CANInfoConstPtr & msg);
-	void callbackGetRobotOdom(const nav_msgs::OdometryConstPtr& msg);
-	void callbackGetVehicleStatus(const autoware_msgs::VehicleStatusConstPtr & msg);
 
 	//Helper Functions
 	void VisualizeLocalTracking();
@@ -212,43 +183,6 @@ public:
   ContourTracker();
   ~ContourTracker();
   void MainLoop();
-
-	//Mapping Section
-	UtilityHNS::MapRaw m_MapRaw;
-  	ros::Subscriber sub_bin_map;
-	ros::Subscriber sub_lanes;
-	ros::Subscriber sub_points;
-	ros::Subscriber sub_dt_lanes;
-	ros::Subscriber sub_intersect;
-	ros::Subscriber sup_area;
-	ros::Subscriber sub_lines;
-	ros::Subscriber sub_stop_line;
-	ros::Subscriber sub_signals;
-	ros::Subscriber sub_vectors;
-	ros::Subscriber sub_curbs;
-	ros::Subscriber sub_edges;
-	ros::Subscriber sub_way_areas;
-	ros::Subscriber sub_cross_walk;
-	ros::Subscriber sub_nodes;
-
-	void callbackGetLanelet2(const autoware_lanelet2_msgs::MapBin& msg);
-	void callbackGetVMLanes(const vector_map_msgs::LaneArray& msg);
-	void callbackGetVMPoints(const vector_map_msgs::PointArray& msg);
-	void callbackGetVMdtLanes(const vector_map_msgs::DTLaneArray& msg);
-	void callbackGetVMIntersections(const vector_map_msgs::CrossRoadArray& msg);
-	void callbackGetVMAreas(const vector_map_msgs::AreaArray& msg);
-	void callbackGetVMLines(const vector_map_msgs::LineArray& msg);
-	void callbackGetVMStopLines(const vector_map_msgs::StopLineArray& msg);
-	void callbackGetVMSignal(const vector_map_msgs::SignalArray& msg);
-	void callbackGetVMVectors(const vector_map_msgs::VectorArray& msg);
-	void callbackGetVMCurbs(const vector_map_msgs::CurbArray& msg);
-	void callbackGetVMRoadEdges(const vector_map_msgs::RoadEdgeArray& msg);
-	void callbackGetVMWayAreas(const vector_map_msgs::WayAreaArray& msg);
-	void callbackGetVMCrossWalks(const vector_map_msgs::CrossWalkArray& msg);
-	void callbackGetVMNodes(const vector_map_msgs::NodeArray& msg);
-	void kmlMapFileNameCallback(const std_msgs::String& file_name);
-	void LoadKmlMap(const std::string& file_name);
-	void LoadMap();
 };
 
 }
